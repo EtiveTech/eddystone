2da088eb86b5ef53c4f29acde5851e75c4f086ec
Phil Crooks <coder.crooks@gmail.com>
2017-11-06 08:24:34 +0000

    max request id factored out for test purposes

--- client/src/model/request_id.js
+++ client/src/model/request_id.js
@@ -1,23 +1,24 @@
 "use strict"
 
+const _maxRequestId = Number.MAX_SAFE_INTEGER;
 let _nextRequestId = 0;
 
 const RequestId = function() {
 	// MAX_SAFE_INTEGER === 9007199254740991
 	// This equates to over 2 billion years of messages if sending one message every 10 seconds (which is realistic)
 	// So don't really have to worry about the wrap around
-	if (_nextRequestId === Number.MAX_SAFE_INTEGER) _nextRequestId = 0;
+	if (_nextRequestId === _maxRequestId) _nextRequestId = 0;
 	_nextRequestId += 1;
 	this._id = _nextRequestId;
 }
 
 RequestId.prototype.greaterThan = function(another) {
-	if ((this._id === 1) && (another._id === Number.MAX_SAFE_INTEGER)) return true;
+	if ((this._id === 1) && (another._id === _maxRequestId)) return true;
 	return (this._id > another._id);
 };
 
 RequestId.prototype.lessThan = function(another) {
-	if ((this._id === Number.MAX_SAFE_INTEGER) && (another._id === 1)) return true;
+	if ((this._id === _maxRequestId) && (another._id === 1)) return true;
 	return (this._id < another._id);
 };
 
